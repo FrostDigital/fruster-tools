f16ed7326634d821041e787c9262bce610c4c3f6
Joel Soderstrom <joel.soderstrom@gmail.com>
2016-11-29 17:35:38 +0100

    wip

--- cli/fruster.js
+++ cli/fruster.js
@@ -7,4 +7,5 @@ program
   .command("run <fruster>", "run local fruster").alias("start") 
   .command("logs <app>", "view logs of deis app").alias("log") 
   .command("config <config...>", "set config on app(s)")  
+  .command("create deis", "an interactive guide to create and configure a version of Deis")  
   .parse(process.argv);
