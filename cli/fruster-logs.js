79c5c5cd810edab3eb0d30dd830607b2b96924f4
Joel Soderstrom <joel.soderstrom@gmail.com>
2016-12-19 22:13:19 +0100

    Fixes logs

--- cli/fruster-logs.js
+++ cli/fruster-logs.js
@@ -4,43 +4,38 @@ const program = require('commander');
 const kube = require('../lib/kube');
 const deis = require('../lib/deis');
 
-program  
-  .option("-t, --tail", "Follow/tail logs")  
+program
+  .option("-t, --tail", "Follow/tail logs")
   .description(`
 Show logs of app(s). Will get logs via kube instead of deis. 
 
 Support wildcard pattern for app name.
-`
-)
+`)
   .parse(process.argv);
 
 const appName = program.args[0];
 const tail = program.tail;
 
-if(!appName) {
+if (!appName) {
   console.log("Missing app name or pattern");
   process.exit(-1);
 }
 
-if(appName.indexOf("*") > -1) {
-  deis.apps(appName)
-    .then(apps => Promise.all(apps.map(showLogs)));  
-} else {
-  showLogs(appName);
-}
+deis.apps(appName)
+  .then(apps => Promise.all(apps.map(showLogs)));
+
 
 function showLogs(app) {
   return kube.logs(app.id, tail)
     .then(logOutput => {
       const header = `==========  ${app.id}  ==========`;
-      const separator = Array(header.length+1).join("=");      
-      
+      const separator = Array(header.length + 1).join("=");
+
       console.log("\n\n");
-      console.log(separator);            
+      console.log(separator);
       console.log(header);
-      console.log(separator);            
+      console.log(separator);
       console.log(logOutput);
     })
-    .catch(console.err);  
+    .catch(console.err);
 }
-
