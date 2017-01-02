9ac86ca2cdc80507e1feedf5298ea213bbf18e76
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-01-02 20:44:39 +0100

    Implements service registry in git

--- cli/fruster-start.js
+++ cli/fruster-start.js
@@ -8,12 +8,19 @@ program
   //.option("--exclude", "Wildcard pattern of services to exclude")
   .option("--verbose", "Verbose logging of build")
   .description(`
-Start fruster locally. Will start all services defined in service registry.
+Start fruster locally. Will start all services defined in service registry that is either a
+path or a git url or relative github path (org/repo).
+
+Will choose default branch in git repo but can be specified by appending "#branchName".
 
 Example:
 
 # Start fruster with services defined in github repo frostdigital/agada in file services.json
 $ fruster start frostdigital/agada
+
+# Start fruster with services defined in github repo in branch develop 
+$ fruster start frostdigital/agada#develop
+
 
 # Start fruster with services defined in local file 
 $ fruster start ~/agada/services.json
