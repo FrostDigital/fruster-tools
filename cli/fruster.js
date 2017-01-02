38e0f96f142531645ddfc86a7a8f04563a4f3ccf
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-01-02 22:38:56 +0100

    Adds get/set/unset healthchecks

--- cli/fruster.js
+++ cli/fruster.js
@@ -4,10 +4,11 @@ var program = require("commander");
 
 program
   .version("0.0.1")  
-  .command("start <service-registry>", "start local fruster defined by service registry")
+  .command("start <service-registry>", "start local fruster defined by service registry").alias("run")
   .command("logs <app>", "view logs of deis app").alias("log") 
   .command("config <config...>", "set/unset/get config for app(s)")  
   .command("use <cluster>", "switch kube and deis cluster").alias("switch")
   .command("add-deis-cluster <cluster>", "add deis cluster")  
-  .command("port-forward <pod> <portMapping>", "port forward localhost to remote pod")  
+  .command("port-forward <pod> <portMapping>", "port forward localhost to remote pod").alias("pf")
+  .command("healthcheck", "set, get or unset healtcheck").alias("hc")  
   .parse(process.argv);
