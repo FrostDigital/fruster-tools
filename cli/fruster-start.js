0c3844fd3b7c53f8e4daa28228aa29bfdfb4f336
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-04-21 13:32:42 +0530

    WIP

--- cli/fruster-start.js
+++ cli/fruster-start.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const conf = require('../conf');
 const runner = require("../lib/runner");
 
 program    
-  //.option("--exclude", "Wildcard pattern of services to exclude")
+  .option("-e, --environment <environment>", "prod|int|stg etc")
   .option("--verbose", "Verbose logging of build")
   .description(`
 Start fruster locally. Will start all services defined in service registry that is either a
@@ -29,10 +28,15 @@ $ fruster start ~/agada/services.json
   .parse(process.argv);
 
 const serviceRegPath = program.args[0];
+const environment = program.environment;
+const verbose = program.verbose;
 
 if(!serviceRegPath) {
   console.error("ERROR: Missing name of fruster to start");
   process.exit(1);
 }
 
-runner.start(serviceRegPath, program.verbose);
+runner.start(serviceRegPath, { 
+	environment: environment,
+	verbose: verbose
+});
