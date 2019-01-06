6addc702416a1baedb3faa40b2f5a28656cda994
Joel Soderstrom <joel.soderstrom@gmail.com>
2019-01-06 14:27:18 +0100

    format

--- cli/fruster-create-apps.js
+++ cli/fruster-create-apps.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const deis = require('../lib/deis');
-const serviceRegistryFactory = require('../lib/service-registry');
+const program = require("commander");
+const deis = require("../lib/deis");
+const serviceRegistryFactory = require("../lib/service-registry");
 const log = require("../lib/log");
 const Promise = require("bluebird");
 
@@ -15,8 +15,9 @@ Example:
 
 # Set BUS on all apps with name that starts with "ag-"
 $ fruster config create-apps frostdigital/paceup
-`)  
-  .option("-d, --dry-run", "just check, no writing")  
+`
+  )
+  .option("-d, --dry-run", "just check, no writing")
   .option("-e, --environment <environment>", "prod|int|stg etc")
   .parse(process.argv);
 
@@ -29,34 +30,33 @@ if (!serviceRegPath) {
   process.exit(1);
 }
 
-serviceRegistryFactory.create(serviceRegPath, { environment: environment }).then(serviceRegistry => {
-  
-  return deis.apps()
-    .then(apps => {      
-      return Promise.mapSeries(serviceRegistry.services, (service) => {
+serviceRegistryFactory
+  .create(serviceRegPath, { environment: environment })
+  .then(serviceRegistry => {
+    return deis.apps().then(apps => {
+      return Promise.mapSeries(serviceRegistry.services, service => {
         let promise = Promise.resolve();
 
-        if(!apps.find(app => app.id == service.name)) {                  
-          log.info(`[${service.name}] Creating app ...`);            
-          
-          if(!dryRun) {
+        if (!apps.find(app => app.id == service.name)) {
+          log.info(`[${service.name}] Creating app ...`);
+
+          if (!dryRun) {
             promise.then(() => deis.createApp(service.name));
-          }            
-       
+          }
         } else {
           log.success(`[${service.name}] Already exists`);
         }
-  
+
         return promise;
-      });            
+      });
     });
-})
-.then((res) => {
-  if(!dryRun) {    
-    log.success(`\n✔ ${res.length} app(s) created`);
-  }
-})
-.catch(err => {
-  console.log(err);
-  process.exit(1);
-});
+  })
+  .then(res => {
+    if (!dryRun) {
+      log.success(`\n✔ ${res.length} app(s) created`);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
