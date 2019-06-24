f185bd836d3f70798a0e11312f3f081512a78433
Joel Soderstrom <joel.soderstrom@gmail.com>
2019-06-24 22:13:12 +0200

    Adds ability to set args which are interpolated, also adds option to extend another service by name

--- spec/service-registry-factory.spec.js
+++ spec/service-registry-factory.spec.js
@@ -6,93 +6,98 @@ const svcReg = require("../lib/service-registry");
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
 
-xdescribe("Service registry", () => {
-
+describe("Service registry", () => {
 	let paceup;
 
 	beforeEach(done => {
 		process.env.AN_ENV_VAR = "12";
 
-		svcReg.create(path.join(__dirname, "support", "fruster-paceup.json"))
-		.then(serviceRegistry => {
-			paceup = serviceRegistry;
-			done();
-		})
-		.catch(done.fail);
+		svcReg
+			.create(path.join(__dirname, "support", "fruster-paceup.json"))
+			.then(serviceRegistry => {
+				paceup = serviceRegistry;
+				done();
+			})
+			.catch(done.fail);
 	});
 
 	afterEach(() => {
 		delete process.env.AN_ENV_VAR;
 	});
 
-	it("should be created from file", done => {
+	it("should be created from file", async () => {
+		const serviceRegistry = await svcReg.create(path.join(__dirname, "support", "fruster-paceup.json"));
 
-		svcReg.create(path.join(__dirname, "support", "fruster-paceup.json"))
-		.then(serviceRegistry => {	
-			expect(serviceRegistry).toBeDefined();
-			expect(serviceRegistry.services.length).toBe(2);
-			expect(serviceRegistry.services[0].env.LOG_LEVEL).toBe("DEBUG");
-			expect(serviceRegistry.services[0].env.FOO).toBe("123");
-			done();
-		})
-		.catch(done.fail);		
+		expect(serviceRegistry).toBeDefined();
+		expect(serviceRegistry.services.length).toBe(2);
+		expect(serviceRegistry.services[0].env.LOG_LEVEL).toBe("DEBUG");
+		expect(serviceRegistry.services[0].env.FOO).toBe("12");
 	});
-
-	it("should be created from git repo", done => {
-		svcReg.create("frostdigital/paceup")
-		.then(serviceRegistry => {			
-			expect(serviceRegistry).toBeDefined();
-			expect(serviceRegistry.services.length).toBeGreaterThan(5);
-			done();
-		})
-		.catch(done.fail);		
-	});
-
 
 	it("should get filtered list of services", () => {
 		expect(paceup.getServices("*api*").length).toBe(1);
 	});
 
 	describe("with inheritance", () => {
-		
 		let serviceRegistry;
 
-		beforeEach((done) => {
-			svcReg.create(path.join(__dirname, "support", "service-registry-inheritance", "extends.json"))
-			.then(_serviceRegistry => {			
-				serviceRegistry = _serviceRegistry;				
-				done();
-			})
-			.catch(done.fail);			
+		beforeEach(async () => {
+			serviceRegistry = await svcReg.create(
+				path.join(__dirname, "support", "service-registry-inheritance", "extends.json")
+			);
 		});
 
 		it("should inherit from extended service registry", () => {
-			expect(serviceRegistry.services.length).toBe(2);
-			expect(serviceRegistry.services[0].env.HELLO_FROM_SUPER).toBe("true");			
+			expect(serviceRegistry.services.length).toBe(3);
+			expect(serviceRegistry.services[0].env.HELLO_FROM_SUPER).toBe("true");
 			expect(serviceRegistry.services[0].env.FOO).toBe("BAR");
 			expect(serviceRegistry.services[1].env.HELLO_FROM_SUPER).toBe("true");
 			expect(serviceRegistry.name).toBe("test");
 
 			let apiGateway = serviceRegistry.services.find(s => s.name === "fruster-api-gateway");
-			expect(apiGateway.env.I_LOVE).toBe("lots of candy");
+			expect(apiGateway.env.GLOBAL_ENV_VAR).toBe("overridden global env var");
 			expect(apiGateway.env.NULL).toBeUndefined();
 		});
 
 		it("should get a flat representation when invoking toJSON()", () => {
 			const json = serviceRegistry.toJSON();
 
-			expect(json.services.length).toBe(2);
-			expect(json.services[0].env.HELLO_FROM_SUPER).toBe("true");			
+			expect(json.services.length).toBe(3);
+			expect(json.services[0].env.HELLO_FROM_SUPER).toBe("true");
 			expect(json.services[0].env.FOO).toBe("BAR");
 			expect(json.services[1].env.HELLO_FROM_SUPER).toBe("true");
-			expect(json.name).toBe("test");			
+			expect(json.name).toBe("test");
+		});
+
+		it("should extend service by name", () => {
+			const json = serviceRegistry.toJSON();
+			const serviceNames = json.services.map(s => s.name);
+
+			expect(json.services.length).toBe(3);
+			expect(serviceNames).toContain("fruster-api-gateway");
+			expect(serviceNames).toContain("fruster-auth-service");
+			expect(serviceNames).toContain("fruster-auth-service-2");
+
+			const frusterAuthService2 = json.services.find(service => service.name === "fruster-auth-service-2");
+
+			expect(frusterAuthService2.env.BAZ).toBe("BAZ");
+			expect(frusterAuthService2.env.GLOBAL_ENV_VAR).toBe("global env var");
+			expect(frusterAuthService2.repo).toBe("http://github.com/frostdigital/fruster-auth-service");
+		});
+
+		it("should interpolate env", () => {
+			const json = serviceRegistry.toJSON();
+			const frusterAuthService2 = json.services.find(service => service.name === "fruster-auth-service-2");
+			expect(frusterAuthService2.env.INTERPOLATED_VALUE).toBe("INTERPOLATED!");
 		});
 	});
 
-	describe("that is cloned or updated", () => {
-
-		beforeEach(done => {			
-			paceup.cloneOrUpdateServices().then(done).catch(done.fail);
+	xdescribe("that is cloned or updated", () => {
+		beforeEach(done => {
+			paceup
+				.cloneOrUpdateServices()
+				.then(done)
+				.catch(done.fail);
 		});
 
 		afterEach(() => {
@@ -109,7 +114,9 @@ xdescribe("Service registry", () => {
 				expect(["fruster-api-gateway", "fruster-auth-service"]).toContain(p.name);
 			});
 
-			paceup.whenAllBuilt().then(() => {
+			paceup
+				.whenAllBuilt()
+				.then(() => {
 					paceup.buildProcesses.forEach(p => {
 						expect(p.exitCode).toBe(0);
 					});
@@ -120,7 +127,7 @@ xdescribe("Service registry", () => {
 
 		it("should start services (and probably fail to do so)", done => {
 			let onDataCounter = 0;
-			paceup.start((data) => {
+			paceup.start(data => {
 				onDataCounter++;
 			});
 
@@ -129,9 +136,9 @@ xdescribe("Service registry", () => {
 			paceup.startedProcesses.forEach(p => {
 				expect(p.exitCode).toBe(null);
 				expect(["fruster-api-gateway", "fruster-auth-service"]).toContain(p.name);
-				expect(p.env.LOG_LEVEL).toBe("DEBUG");				
+				expect(p.env.LOG_LEVEL).toBe("DEBUG");
 			});
-			
+
 			setTimeout(() => paceup.startedProcesses.forEach(expectRunningOrTerminatedProcess), 1000);
 
 			function expectRunningOrTerminatedProcess(p) {
@@ -140,7 +147,5 @@ xdescribe("Service registry", () => {
 				done();
 			}
 		});
-
 	});
-
 });
