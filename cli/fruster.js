8c2c71f1fe6a87d7c8f51abb6813900d2aa3b892
Joel Soderstrom <joel.soderstrom@gmail.com>
2019-11-28 19:07:50 +0100

    fruster apps

--- cli/fruster.js
+++ cli/fruster.js
@@ -7,6 +7,7 @@ program
 	.command("config", "set/unset/get config for app(s)")
 	.command("create", "create deis apps defined in service registry")
 	.command("scale", "scale number of replicas of a deployment")
+	.command("apps", "list apps/services")
 	.command("deis", "deprecated ")
 	.alias("d")
 	.parse(process.argv);
