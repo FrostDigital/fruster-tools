c5a38c1e3ba4eea7de750b5a6bb85f649afe4037
Joel Soderstrom <joel.soderstrom@gmail.com>
2019-12-05 21:36:49 +0100

    Code cleaning

--- cli/fruster-create.js
+++ cli/fruster-create.js
@@ -4,7 +4,7 @@ const program = require("commander");
 const serviceRegistryFactory = require("../lib/service-registry");
 const { createDeployment, createService, createNamespace, copySecret, setConfig } = require("../lib/kube/kube-client");
 const log = require("../lib/log");
-const { validateRequiredArg } = require("../lib/utils/cli-utils");
+const { validateRequiredArg, getUsername } = require("../lib/utils/cli-utils");
 
 program
 	.description(
@@ -34,44 +34,41 @@ validateRequiredArg(serviceRegPath, program, "Missing service registry path");
 validateRequiredArg(namespace, program, "Missing namespace");
 
 async function run() {
-	try {
-		const serviceRegistry = await serviceRegistryFactory.create(serviceRegPath);
+	const username = await getUsername();
 
-		const apps = serviceRegistry.getServices(app || "*");
+	const serviceRegistry = await serviceRegistryFactory.create(serviceRegPath);
 
-		if (!apps.length) {
-			log.error("Could not find configuration for services " + app);
-			return process.exit(1);
+	const apps = serviceRegistry.getServices(app || "*");
+
+	if (!apps.length) {
+		log.error("Could not find configuration for services " + app);
+		return process.exit(1);
+	}
+
+	for (const appConfig of apps) {
+		if (!appConfig.image) {
+			log.warn("No 'image' set for service " + appConfig.name);
+			continue;
 		}
+		// Upsert namespace
+		await createNamespace(appConfig.name, dryRun);
 
-		for (const appConfig of apps) {
-			if (!appConfig.image) {
-				log.warn("No 'image' set for service " + appConfig.name);
-				continue;
-			}
-			// Upsert namespace
-			await createNamespace(appConfig.name, dryRun);
+		// Copy existing imagePullSecret from default namespace services namespace
+		await copySecret(appConfig.imagePullSecret || "regcred", "default", appConfig.name);
 
-			// Copy existing imagePullSecret from default namespace services namespace
-			await copySecret(appConfig.imagePullSecret || "regcred", "default", appConfig.name);
+		// Upsert config (saved as secets)
+		await setConfig(namespace, appConfig.name, appConfig.env);
 
-			// Upsert config (saved as secets)
-			await setConfig(namespace, appConfig.name, appConfig.env);
+		// Upsert deployment based on configuration from service registry
+		await createDeployment(namespace, appConfig, username + " created app");
 
-			// Upsert deployment based on configuration from service registry
-			await createDeployment(namespace, appConfig);
+		log.success("Created deployment");
 
-			log.success("Created deployment");
-
-			if (appConfig.routable) {
-				log.info("Service is routable, making sure that service exists...");
-				const created = await createService(namespace, appConfig);
-				log.success(`Service ${created ? "was created" : "already exists"}`);
-			}
+		if (appConfig.routable) {
+			log.info("Service is routable, making sure that service exists...");
+			const created = await createService(namespace, appConfig);
+			log.success(`Service ${created ? "was created" : "already exists"}`);
 		}
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
 	}
 }
 
